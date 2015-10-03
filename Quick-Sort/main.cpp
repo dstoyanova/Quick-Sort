db7a0f42e7588a6dec56c90682dae3090f4aac7d
Desislava Stoyanova <dstoyanova@desislavas-macbook-air.local>
2015-10-03 15:47:45 +0200

    Initial Commit

---
+++ Quick-Sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quick-Sort
+//
+//  Created by Desislava Stoyanova on 10/3/15.
+//  Copyright © 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
